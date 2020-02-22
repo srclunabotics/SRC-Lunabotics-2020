679c9594a7d7b5bd82543118263973e7bd680196
Benny Mirisola <bennymirisola@gmail.com>
2020-02-22 14:09:39 -0500

    added TF gazebo subsrciber

--- src/husky_sims/src/TF_broadcaster.py
+++ src/husky_sims/src/TF_broadcaster.py
@@ -3,34 +3,28 @@
 from tf import TransformBroadcaster
 import rospy
 from rospy import Time 
-from gazebo_msgs.srv import GetLinkState
-from geometry_msgs.msg import PoseStamped
+from gazebo_msgs.msg import ModelStates
+
 
 def main():
     rospy.init_node('TF_broadcaster')
+
+def tf_callback(msg):
+    name = msg.name
+    robot_index = name.index("/")
+    pose = msg.pose[robot_index]
     
     b = TransformBroadcaster()
     
-    translation = (0.0, 0.0, 0.0)
-    rotation = (0.0, 0.0, 0.0, 1.0)
-    rate = rospy.Rate(5)  # 5hz
+    rotation = (pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w)
     
-    x, y = 0.0, 0.0
+    translation = (pose.position.x, pose.position.y, pose.position.z)   
+    b.sendTransform(translation, rotation, Time.now(), 'Our special Mining Bot', '/world')
     
-    while not rospy.is_shutdown():
-        if x >= 2:
-            x, y = 0.0, 0.0 
-
-        x += 0.1
-        y += 0.1
-        
-        translation = (x, y, 0.0)
-        
-        
-        b.sendTransform(translation, rotation, Time.now(), 'Our special Mining Bot', '/world')
-        rate.sleep()
-    
-
-
+# topic name: /gazebo/model_states
+# message: ModelStates
 if __name__ == '__main__':
     main()
+
+    rospy.Subscriber('/gazebo/model_states', ModelStates, tf_callback)     
+    rospy.spin()
